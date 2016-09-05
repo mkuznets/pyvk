d6633e6e9c1f731477ac41462a4ebc7ea4b496a7
Maxim Kuznetsov <maks.kuznetsov@gmail.com>
2016-09-05 22:43:25 +0300

    Add setuptools files

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+setup(
+    name='pyvk',
+    version='0.0.1',
+    platforms='any',
+    packages=['pyvk'],
+
+    install_requires=[
+        'requests',
+        'lxml',
+        'appdirs',
+    ],
+
+    author='Max Kuznetsov',
+    author_email='maks.kuznetsov@gmail.com',
+    description='VK API for Python',
+    license='MIT',
+    url='https://github.com/mkuznets/PyVK',
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Build Tools',
+        'Topic :: Internet',
+        'License :: OSI Approved :: MIT License',
+    ],
+)
